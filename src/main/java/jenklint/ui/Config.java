ca497ace49a067d35e742df5eb7e7336fd49e0c0
henryborchers@yahoo.com <henryborchers@yahoo.com>
2020-01-08 21:57:57 -0600

    persist data

--- src/main/java/jenklint/ui/Config.java
+++ src/main/java/jenklint/ui/Config.java
@@ -13,7 +13,7 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
+import com.intellij.ide.util.PropertiesComponent;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,22 +21,52 @@ import java.awt.event.ActionListener;
 public class Config implements Configurable {
     private JPanel rootPanel;
     private TextFieldWithBrowseButton jenklintCommand;
-    private JTextField textField1;
+    private JTextField JenkinsURL;
+    private TextFieldWithBrowseButton jenkinsFile;
     public final String DISPLAY_NAME = "Jenklint";
     private Project project;
+    private boolean modified = false;
 
-//    public Config() {
+    //    public Config() {
     public Config(@NotNull Project project) {
         this.project = project;
+        jenkinsFile.setText(PropertiesComponent.getInstance().getValue("jenkinsfile"));
+        JenkinsURL.setText(PropertiesComponent.getInstance().getValue("jenkinsURL"));
+        jenklintCommand.setText(PropertiesComponent.getInstance().getValue("jenklintCommand"));
+
         jenklintCommand.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("something");
                 final FileChooserDescriptor d = FileChooserDescriptorFactory.createSingleFileDescriptor();
                 VirtualFile initialFile = StringUtil.isNotEmpty(jenklintCommand.getText()) ? LocalFileSystem.getInstance().findFileByPath(jenklintCommand.getText()) : null;
                 VirtualFile file = FileChooser.chooseFile(d, project, initialFile);
                 if (file != null) {
                     jenklintCommand.setText(file.getCanonicalPath());
+                    modified = true;
+                }
+            }
+        });
+
+        JenkinsURL.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("Selecting Jenkins URL");
+            }
+        });
+
+        jenkinsFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                final FileChooserDescriptor d = FileChooserDescriptorFactory.createSingleFileDescriptor();
+
+                VirtualFile initialFile =
+                        StringUtil.isNotEmpty(jenkinsFile.getText()) ? LocalFileSystem.getInstance().findFileByPath(jenkinsFile.getText()) : null;
+
+                VirtualFile file = FileChooser.chooseFile(d, project, initialFile);
+
+                if (file != null) {
+                    jenkinsFile.setText(file.getCanonicalPath());
+                    modified = true;
                 }
             }
         });
@@ -56,11 +86,13 @@ public class Config implements Configurable {
 
     @Override
     public boolean isModified() {
-        return false;
+        return this.modified;
     }
 
     @Override
     public void apply() throws ConfigurationException {
-
+        PropertiesComponent.getInstance().setValue("jenkinsfile", jenkinsFile.getText());
+        PropertiesComponent.getInstance().setValue("jenklintCommand", jenklintCommand.getText());
+        PropertiesComponent.getInstance().setValue("jenkinsURL", JenkinsURL.getText());
     }
 }
