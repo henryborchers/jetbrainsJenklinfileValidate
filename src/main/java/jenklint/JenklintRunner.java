33f8c709ad38d06ac3ad9321f8adbaccd46a037d
henryborchers@yahoo.com <henryborchers@yahoo.com>
2020-01-17 21:27:57 -0600

    ascii char

--- src/main/java/jenklint/JenklintRunner.java
+++ src/main/java/jenklint/JenklintRunner.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class JenklintRunner {
@@ -30,8 +31,9 @@ public class JenklintRunner {
         // TODO: replace with ProcessBuilder b = new ProcessBuilder("C:\DoStuff.exe -arg1 -arg2");
         try {
             Process p = b.start();
+            InputStreamReader inputStreamReader;
 
-            InputStreamReader inputStreamReader = new InputStreamReader(p.getInputStream());
+            inputStreamReader = new InputStreamReader(p.getInputStream(), StandardCharsets.US_ASCII);
             BufferedReader reader = new BufferedReader(inputStreamReader);
             BufferedReader stderor = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String line;
